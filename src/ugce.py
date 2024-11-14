5e3d2577b6dbf9c9ff93997fa026cca9b8361790
xristosfrag <37181592+xristosfrag@users.noreply.github.com>
2024-11-14 13:00:02 +0200

    UPDATE ranges_score

--- src/ugce.py
+++ src/ugce.py
@@ -197,7 +197,7 @@ class UGCE:
                         print("Feature {} is within bounds. Reward: {}".format(attribute_index, ranges_score))
                 else:
                     # Apply penalty if the feature is outside the bounds
-                    ranges_score += 1000 * abs(x_prime[attribute_index] - lower) if x_prime[attribute_index] < lower else abs(x_prime[attribute_index] - upper)
+                    ranges_score -= 1000 * abs(x_prime[attribute_index] - lower) if x_prime[attribute_index] < lower else abs(x_prime[attribute_index] - upper)
                     if verbose:
                         print("Feature {} is outside bounds. Penalty: {}".format(attribute_index, ranges_score))
 

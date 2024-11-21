c7c6448401fd6fb1df3c1144ce4c0eb49f7acf23
xristosfrag <xristosfragath@gmail.com>
2024-11-21 16:52:03 +0200

    RENAME function

--- src/utils.py
+++ src/utils.py
@@ -24,7 +24,7 @@ def scale_constraints(original_constraints, scaler, feature_columns):
     return {i: (scaled_constraints[0][i], scaled_constraints[0][i]) for i in range(len(feature_columns))}
 
 # Train a binary classification model on the COMPAS dataset
-def train_compas_model(features, target):
+def train_test_split_model(features, target):
     X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
     model = LogisticRegression(random_state=42)
     model.fit(X_train, y_train)

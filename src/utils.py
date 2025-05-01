10549772d7f6faa6ff77dcf177a602c785c16645
xristosfrag <xristosfragath@gmail.com>
2025-05-01 12:07:29 +0300

    ADD calculate distances

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,50 @@ from prettytable import PrettyTable
 from scipy.spatial import distance
 import pandas as pd
 import numpy as np
+
+def fast_max_l2_l1_distance(data, sample_size=40000, seed=42):
+    if len(data) > sample_size:
+        np.random.seed(seed)
+        sample_indices = np.random.choice(len(data), sample_size, replace=False)
+        sample = data[sample_indices]
+    else:
+        sample = data
+    return np.max(pdist(sample, metric='euclidean')), np.max(pdist(sample, metric='cityblock'))
+
+
+def compute_distances_in_blocks(data, block_size=1000, representation=16):
+    """Computes distances in smaller blocks and rounds results to save memory.
+    
+    Understanding Memory Usage:
+    Supposing a dataset of 40,000 samples.
+    The pairwise distance matrix is 40,000 x 40,000.
+    Since storing distances in a float64 (8 bytes per value), the total memory required is:
+    40,000 x 40,000 x 8 bytes = 12.8 GB (approximately).
+    
+    Expected Memory Savings
+        Data Type	Memory per Value	Full Matrix Size
+        float64	    8 bytes	            12.8 GB
+        float32	    4 bytes	            6.4 GB
+        float16	    2 bytes	            3.2 GB
+    """
+    n = len(data)
+    if representation == 16:
+        decimals = np.float16
+    elif representation == 32:
+        decimals = np.float32
+    elif representation == 64:
+        decimals = np.float64
+    
+    results = np.zeros((n, n), dtype=decimals)
+    for i in range(0, n, block_size):
+        for j in range(i, n, block_size):
+            i_end = min(i + block_size, n)
+            j_end = min(j + block_size, n)
+            results[i:i_end, j:j_end] = distance.cdist(data[i:i_end], data[j:j_end], 'euclidean').astype(decimals)
+            if i != j:
+                results[j:j_end, i:i_end] = results[i:i_end, j:j_end].T  # Symmetric assignment
+    return results
+
 def transform_individual(individual, scaler):
     return scaler.transform(individual.reshape(1, -1))
 
